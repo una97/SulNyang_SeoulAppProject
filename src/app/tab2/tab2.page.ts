ce06c81e8c99879ffaa76b2adc03f7a7addb6e51
una97 <yoona607@dongguk.edu>
2019-09-14 15:27:12 +0900

    게시판 기본 틀

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit, ViewChild } from '@angular/core';
+import {IonSegment} from '@ionic/angular';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
@@ -7,6 +7,14 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+ @ViewChild(IonSegment) segment: IonSegment;
   constructor() {}
+  ngOnInit(){
+ this.segment.value='help';
+  }
 
+  segmentChanged(event){
+    const valueSeg = event.detail.value;
+    console.log(valueSeg);
+  }
 }
