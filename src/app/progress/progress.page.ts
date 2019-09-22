107972b0ca82201cfd0331b39212e8df1fdc067d
lee minju <dptmfkdls120@gmail.com>
2019-09-22 19:00:28 +0900

    more graph sample

--- src/app/progress/progress.page.ts
+++ src/app/progress/progress.page.ts
@@ -13,7 +13,14 @@ export class ProgressPage implements OnInit {
   public doughnutChartData: number[] = [1200, 2000];
   public doughnutChartType = 'doughnut';
 
-  ngOnInit() {
+  public labels: string[] = ['길고양이', '힉대', '사고', '기타'];
+  public chartData: number[] = [20, 15, 40, 25, 0];
+  public chartType = 'bar';
+
+  public labels2: string[] = ['1월 ', '2월', '3월', '4월', '5월', '6월', '7월','8월', '9월', '10월', '11월', '12월'];
+  public chartData2: number[] = [20, 15, 40, 25, 100, 20, 30, 60, 70, 80, 50, 12, 0];
+  public chartType2 = 'line';
+   ngOnInit() {
   }
 
 }
