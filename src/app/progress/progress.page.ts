bef639c718944923d186fa99f56dc2725c769920
lee minju <dptmfkdls120@gmail.com>
2019-09-22 21:04:45 +0900

    read data from firebase
    abuse, accident, straycats, other.

--- src/app/progress/progress.page.ts
+++ src/app/progress/progress.page.ts
@@ -15,18 +15,22 @@ export class ProgressPage implements OnInit {
 
   // 처리한 건수/ 처리해야할 건수
  public tempDoughnutData: number[] = [0, 0];
+ // 학대, 사고, 길, 기타
+ public tempClassification: number[] = [0, 0, 0, 0];
  // tslint:disable-next-line: variable-name
  public temp_completeNum = 0;
  // tslint:disable-next-line: variable-name
  public temp_wholeNum = 0;
+ public temp_wabuse = 0;
+ public temp_waccident = 0;
+ public temp_wcat =0;
+ public temp_wother =0;
   constructor() {}
 
   public doughnutChartLabels: string[] = ['처리한 건수', '처리해야할 건수'];
-  public doughnutChartData: number[] = [1200, 2000];
   public doughnutChartType = 'doughnut';
 
-  public labels: string[] = ['길고양이', '힉대', '사고', '기타'];
-  public chartData: number[] = [20, 15, 40, 25, 0];
+  public labels: string[] = ['힉대', '사고', '길고양이', '기타'];
   public chartType = 'bar';
 
   public labels2: string[] = ['1월 ', '2월', '3월', '4월', '5월', '6월', '7월','8월', '9월', '10월', '11월', '12월'];
@@ -45,6 +49,24 @@ export class ProgressPage implements OnInit {
           this.temp_wholeNum = k;
           console.log(this.temp_wholeNum);
           this.tempDoughnutData = [this.temp_completeNum, this.temp_wholeNum];
+
+          let a = snapshot.child(`seoulGu/wabuse`).val(); // 전체 학대 받는 고양이 수
+          this.temp_wabuse = a;
+          console.log(this.temp_wabuse);
+
+          let b = snapshot.child(`seoulGu/waccident`).val(); // 전체 사고 고양이 수
+          this.temp_waccident = b;
+          console.log(this.temp_waccident);
+
+          let d = snapshot.child(`seoulGu/wcat`).val(); // 전체 학대 받는 고양이 수
+          this.temp_wcat = a;
+          console.log(this.temp_wcat);
+
+          let e = snapshot.child(`seoulGu/wother`).val(); // 전체 학대 받는 고양이 수
+          this.temp_wother = a;
+          console.log(this.temp_wother);
+
+          this.tempClassification = [this.temp_wabuse, this.temp_waccident, this.temp_wcat, this.temp_wother];
     });
   }
 
