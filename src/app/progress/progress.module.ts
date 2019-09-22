6d723847cd569502f470e2245dc6128adf0feb5c
lee minju <dptmfkdls120@gmail.com>
2019-09-22 18:29:32 +0900

    add graph

--- src/app/progress/progress.module.ts
+++ src/app/progress/progress.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
+import {ChartsModule} from 'ng2-charts';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -19,6 +20,7 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
+    ChartsModule,
     RouterModule.forChild(routes)
   ],
   declarations: [ProgressPage]
--- src/app/progress/progress.page.ts
+++ src/app/progress/progress.page.ts
@@ -9,6 +9,10 @@ export class ProgressPage implements OnInit {
 
   constructor() { }
 
+  public doughnutChartLabels: string[] = ['처리한 건수', '처리해야할 건수'];
+  public doughnutChartData: number[] = [1200, 2000];
+  public doughnutChartType = 'doughnut';
+
   ngOnInit() {
   }
 
