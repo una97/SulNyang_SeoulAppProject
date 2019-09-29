9bf4e4e18083643031450dfd0d26da7e6695c983
ha-yujin <hoj2887@dongguk.edu>
2019-09-29 18:20:58 +0900

    when no chatting

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -5,6 +5,7 @@ import { NavController, AlertController } from '@ionic/angular';
 import { Storage } from '@ionic/storage';
 import * as firebase from 'firebase';
 import { Router } from '@angular/router';
+import { AngularFireDatabase } from 'angularfire2/database';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
@@ -15,25 +16,45 @@ export class Tab3Page {
   chattingRef: any;
   ID: string;
   you: string;
+  public check=false;
   constructor(
     public fs: AngularFirestore,
     public af: AngularFireAuth,
     public nav: NavController,
     public router: Router,
     public stor: Storage,
-    public atrCtrl: AlertController
+    public atrCtrl: AlertController,
+    public db: AngularFireDatabase
     ) {
       // this.Email=this.af.auth.currentUser.email,
       this.stor.get('id').then((val) => {
         this.ID = val;
       });
       this.chattingRef = this.fs.collection('chatting', ref => ref.orderBy('Timestamp')).valueChanges();
+      const DB=firebase.firestore();
+      const collection = DB.collection('chatting');
+
+      collection.get().then(snapshot=>{
+        snapshot.forEach(doc=>{
+          const get1=doc.data().uid1;
+          const get2=doc.data().uid2;
+          console.log(get1+"    "+get2);
+          if((this.ID === get1)||(this.ID===get2)){
+            this.check=true;
+          }
+          console.log("체크"+this.check);
+        });
+      });
+      console.log("없음");
   }
 
   ionViewWillEnter() {
     this.stor.get('id').then((val) => {
       this.ID = val;
     });
+  }
+  ngOnInit(){
+    
   }
   openChat(you: string) {
     this.you = you;
