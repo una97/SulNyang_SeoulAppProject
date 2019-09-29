92d0e4cf217ef3d490fdb25f887a7680c12e36bf
lee minju <dptmfkdls120@gmail.com>
2019-09-29 22:23:43 +0900

    처리현황페이지

--- src/app/progress/progress.page.ts
+++ src/app/progress/progress.page.ts
@@ -28,17 +28,15 @@ export class ProgressPage implements OnInit {
  public fr = 0;
  public temp_guName: string;
  informGu:string = '';
-  constructor() {}
+  constructor(
+    public db: AngularFireDatabase
+) {}
 
-  public doughnutChartLabels: string[] = ['처리한 건수', '처리해야할 건수'];
+  public doughnutChartLabels: string[] = ['처리 건수', '미처리 건수'];
   public doughnutChartType = 'doughnut';
 
   public labels: string[] = ['힉대', '사고', '길고양이', '기타'];
   public chartType = 'bar';
-
-  public labels2: string[] = ['1월 ', '2월', '3월', '4월', '5월', '6월', '7월','8월', '9월', '10월', '11월', '12월'];
-  public chartData2: number[] = [20, 15, 40, 25, 100, 20, 30, 60, 70, 80, 50, 12, 0];
-  public chartType2 = 'line';
 
    ngOnInit() {
      /*처리할 건수 처리해야할 건수 firebase에서 데이터 읽어오기*/
@@ -54,7 +52,12 @@ export class ProgressPage implements OnInit {
           this.tempDoughnutData = [this.temp_completeNum, this.temp_wholeNum - this.temp_completeNum];
 
           this.fr = this.temp_completeNum / this.temp_wholeNum * 100;
-          console.log('fr', this.fr);
+          // tslint:disable-next-line: triple-equals
+          // tslint:disable-next-line: use-isnan
+          if (this.fr === NaN) {
+              this.fr = 0;
+          }
+
           let a = snapshot.child(`seoulGu/wabuse`).val(); // 전체 학대 받는 고양이 수
           this.temp_wabuse = a;
           console.log(this.temp_wabuse);
@@ -64,11 +67,11 @@ export class ProgressPage implements OnInit {
           console.log(this.temp_waccident);
 
           let d = snapshot.child(`seoulGu/wcat`).val(); // 전체 길고양이? 고양이 수
-          this.temp_wcat = a;
+          this.temp_wcat = d;
           console.log(this.temp_wcat);
 
           let e = snapshot.child(`seoulGu/wother`).val(); // 전체 기타 고양이 수
-          this.temp_wother = a;
+          this.temp_wother = e;
           console.log(this.temp_wother);
 
           this.tempClassification = [{data: [this.temp_wabuse, this.temp_waccident, this.temp_wcat, this.temp_wother], label: '제보수'}];
@@ -78,6 +81,12 @@ export class ProgressPage implements OnInit {
     firebase.database().ref().once('value').then((snapshot) => {
       // tslint:disable-next-line: prefer-const
       if ( this.informGu === '서울 전체') {
+        let c = snapshot.child('seoulGu/completeNum').val();  //전체 제보 처리 완료 수
+        this.temp_completeNum = c;
+      // tslint:disable-next-line: prefer-const
+        let k = snapshot.child(`seoulGu/wholeNum`).val(); // 전체 제보 받은 수
+        this.temp_wholeNum = k;
+
         let a = snapshot.child(`seoulGu/wabuse`).val(); // 전체 학대 받는 고양이 수
         this.temp_wabuse = a;
         console.log(this.temp_wabuse);
@@ -87,38 +96,46 @@ export class ProgressPage implements OnInit {
         console.log(this.temp_waccident);
 
         let d = snapshot.child(`seoulGu/wcat`).val(); // 전체 길고양이? 고양이 수
-        this.temp_wcat = a;
+        this.temp_wcat = d;
         console.log(this.temp_wcat);
 
         let e = snapshot.child(`seoulGu/wother`).val(); // 전체 기타 고양이 수
-        this.temp_wother = a;
+        this.temp_wother = e;
         console.log(this.temp_wother);
 
+        this.fr = this.temp_completeNum / this.temp_wholeNum * 100;
+        this.tempDoughnutData = [this.temp_completeNum, this.temp_wholeNum - this.temp_completeNum];
         this.tempClassification = [{data: [this.temp_wabuse, this.temp_waccident, this.temp_wcat, this.temp_wother], label: '제보수'}];
       } else {
       for (this.i = 0; this.i < 25; this.i++) {
+
           let c = snapshot.child(`seoulGu/${this.i}/guName`).val();  // 구 이름
           this.temp_guName = c;
-          console.log('this.temp_guName', this.temp_guName);
 
           if(this.temp_guName === this.informGu) { // 선택한 구의 정보 찾기
             this.j = this.i;
             let a = snapshot.child(`seoulGu/${this.j}/abuse`).val(); // 해당 구 학대 받는 고양이 수
             this.temp_wabuse = a;
-            console.log(this.temp_wabuse);
 
             let b = snapshot.child(`seoulGu/${this.j}/accident`).val(); // 해당 구 사고 고양이 수
             this.temp_waccident = b;
-            console.log(this.temp_waccident);
 
             let d = snapshot.child(`seoulGu/${this.j}/cat`).val(); //  해당 구 길고양이? 고양이 수
             this.temp_wcat = a;
-            console.log(this.temp_wcat);
 
             let e = snapshot.child(`seoulGu/${this.j}/other`).val(); // 해당 구 기타 고양이 수
             this.temp_wother = a;
-            console.log(this.temp_wother);
 
+            /* 구 별 처리건수*/
+            let s = snapshot.child(`seoulGu/${this.j}/GuComplete`).val();  //구별 제보 처리 완료 수
+            this.temp_completeNum = s;
+    
+           // tslint:disable-next-line: prefer-const
+            let k = snapshot.child(`seoulGu/${this.j}/informNum`).val(); // 구별 제보 받은 수
+            this.temp_wholeNum = k;
+
+            this.fr = this.temp_completeNum / this.temp_wholeNum * 100;
+            this.tempDoughnutData = [this.temp_completeNum, this.temp_wholeNum - this.temp_completeNum];
             this.tempClassification = [{data: [this.temp_wabuse, this.temp_waccident, this.temp_wcat, this.temp_wother], label: '제보수'}];
             break;
           }
--- src/app/showinform/showinform.page.ts
+++ src/app/showinform/showinform.page.ts
@@ -24,8 +24,16 @@ public itemtmp: any;
 writer: string;
 temp_content: number;
 temp_complete: number;
+temp_Gucomplete: number;
+temp_GU: string;
+GU: any;
+GUU:number;
 comment: string;
 text: string;
+temp_com: number = 0;
+i = 0;
+j =0;
+test_GU:string;
 constructor(
   public navCtrl: NavController,
   public atrCtrl: AlertController,
@@ -49,15 +57,42 @@ constructor(
         this.temp_complete = c;
         let co = snapshot.child(`informTxt/${this.code}/comment`).val();
         this.comment = co;
+        let a = snapshot.child(`informTxt/${this.code}/gu`).val();
+        this.temp_GU = a;
+        console.log(this.temp_GU);
   });
   this.load();
 }
 async done() {
-  console.log(this.code);
+  if(this.comment == null || this.comment === '') {
+    this.comment = '작성한 답변이 없습니다.';
+  }
   firebase.database().ref().once('value').then((snapshot) => {
+    // tslint:disable-next-line: triple-equals
     this.db.object(`informTxt/${this.code}/complete`).set(1);   // 확인 완료 버튼 누르면 complete가 1 되게
     this.db.object(`informTxt/${this.code}/comment`).set(this.comment);
+    let c = snapshot.child(`seoulGu/completeNum`).val();
+    this.temp_complete = c;
+    this.temp_complete = this.temp_complete + 1;
+    this.db.object(`seoulGu/completeNum`).set(this.temp_complete);
   });
+
+  firebase.database().ref().once('value').then((snapshot) => {
+      for (this.i = 0; this.i < 25; this.i++) {
+        let g = snapshot.child(`seoulGu/${this.i}/guName`).val();
+        this.test_GU = g;
+
+        if(this.test_GU === this.temp_GU) 
+        {
+          this.j = this.i
+          let c = snapshot.child(`seoulGu/${this.j}/GuComplete`).val();
+          this.temp_complete = c;
+          this.temp_complete = this.temp_complete + 1;
+          this.db.object(`seoulGu/${this.j}/GuComplete`).set(this.temp_complete);
+        }
+      }
+  });
+
   this.atrCtrl.create({
     header: '알림',
     message: '확인 완료 되었습니다.',
@@ -75,7 +110,26 @@ cancel() {
   firebase.database().ref().once('value').then((snapshot) => {
     this.db.object(`informTxt/${this.code}/complete`).set(0);   // 확인 취소 버튼 누르면 complete가 1 되게
     this.db.object(`informTxt/${this.code}/comment`).set('');
+    let c = snapshot.child(`seoulGu/completeNum`).val();
+    this.temp_complete = c;
+    this.temp_complete = this.temp_complete - 1;
+    this.db.object(`seoulGu/completeNum`).set(this.temp_complete);
   });
+  firebase.database().ref().once('value').then((snapshot) => {
+    for (this.i = 0; this.i < 25; this.i++) {
+      let g = snapshot.child(`seoulGu/${this.i}/guName`).val();
+      this.test_GU = g;
+
+      if(this.test_GU === this.temp_GU) 
+      {
+        this.j = this.i
+        let c = snapshot.child(`seoulGu/${this.j}/GuComplete`).val();
+        this.temp_complete = c;
+        this.temp_complete = this.temp_complete - 1;
+        this.db.object(`seoulGu/${this.j}/GuComplete`).set(this.temp_complete);
+      }
+    }
+});
   this.atrCtrl.create({
     header: '알림',
     message: '확인 취소 되었습니다.',
@@ -86,6 +140,7 @@ cancel() {
   }).then(alertEl => {
     alertEl.present();
   });
+  this.comment = null;
   this.router.navigateByUrl('cinform');
 }
 /* 제보 취소 */
@@ -113,6 +168,7 @@ load() {
       this.item = data;
       this.itemtmp = data[0];
       this.headert = this.itemtmp.category;
+      
      // this.temp_code = data[0].code;
       this.db.list('userInfo/', ref => ref.orderByChild('userid').equalTo(this.writer)).valueChanges().subscribe(
         // tslint:disable-next-line:no-shadowed-variable
